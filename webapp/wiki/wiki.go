f7bf062b3d6d31e608588752405a2402c5a5540b
takakd <takakd@takakd.com>
2020-03-20 23:06:10 +0900

    add: webapp. to section"Editing Pages"

---
+++ webapp/wiki/wiki.go
@@ -0,0 +1,61 @@
+// +build ignore
+
+package main
+
+import (
+	"net/http"
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	log.Println(body, err)
+	return &Page{Title: title, Body: body}, nil
+}
+
+//func handler(w http.ResponseWriter, r *http.Request) {
+//	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+//}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, err := loadPage(title)
+	log.Println(p, err)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	fmt.Fprintf(w,
+		"<h1>Editing %s</h1>"+
+			"<form action=\"/save/%s\" method=\"POST\">"+
+			"<textarea name=\"body\">%s</textarea><br>"+
+			"<input type=\"submit\" value=\"Save\">"+
+			"</form>",
+		p.Title, p.Title, p.Body)
+}
+
+func main() {
+	//http.HandleFunc("/", handler)
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	//http.HandleFunc("/save/", saveHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
